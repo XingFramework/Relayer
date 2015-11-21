4739044a9b656f1bc3bda9c14aeec5e4e5053a70
Hannah Howard <hannah@hannahhoward.net>
2015-11-20 17:13:25 -0800

    remove dist files

--- dist/relayer/endpoints.js
+++
@@ -1,3 +0,0 @@
-export {default as PromiseEndpoint} from "./endpoints/PromiseEndpoint.js";
-export {default as ResolvedEndpoint} from "./endpoints/ResolvedEndpoint.js";
-export {default as LoadedDataEndpoint} from "./endpoints/LoadedDataEndpoint.js";
